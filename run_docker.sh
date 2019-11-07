556bc12cc723bfb20d359ae2d25ec2ef7458fcda
Drew Pearce <drew@caffdev.com>
2019-11-07 12:09:59 -0500

    update docker items

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+# get current dir
 DIR=$(pwd -P)
+
+# get relative path to source dir
 FILE_DIR=`dirname ${BASH_SOURCE[0]}`
+
+# save full path source dir
 cd $FILE_DIR
 FILE_DIR=$(pwd -P)
+
+# go back to where we came from
 cd $DIR
 
-docker run -it --rm -v $DIR:/tmp/repo challenge-2019-09:latest
+# run the container, mount the source dir into the container
+docker run -it --rm -v $FILE_DIR:/home/repo drewpearce/trd-challenge:latest
